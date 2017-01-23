ab9c65382bf16869bf1e71123952d9e0f254ad3f
Callie Tomlinson & Sean Wright <pair+callielynne3.nightangelblade@devbootcamp.com>
2017-01-23 14:28:28 -0800

    Edit with controllers for categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,31 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category
+    else
+      render new_category_path
+    end
+  end
+
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
 end
